fd380e979253cbdcc45fd28473c7729b4be5c628
Natasha <njohnson533@gmail.com>
2021-11-17 13:09:51 -0500

    Initial commit, deploy site to github

---
+++ src/Books.js
@@ -0,0 +1,21 @@
+export const books = [
+	{
+		id: 1,
+		img: "https://m.media-amazon.com/images/I/71951W96oWL._AC_UY218_.jpg",
+		author: "Robert Greene",
+		title: "48 Laws of Power",
+	},
+	{
+		id: 2,
+		img: "https://m.media-amazon.com/images/I/81KcsnpAvWL._AC_UY218_.jpg",
+		author: "Anton V. Spraul",
+		title: "Think Like a Programmer",
+	},
+	{
+		id: 3,
+		img: "https://m.media-amazon.com/images/I/91asIC1fRwL._AC_UY218_.jpg",
+		author: "Marijn Haverbeke",
+		title: "Eloquent Javascript",
+	},
+];
+
